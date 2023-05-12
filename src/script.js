a2dc645de25ef13659ab53f155261e843373d4bf
Staffan Ösp <staffan.osp@gmail.com>
2023-05-12 11:25:38 +0200

    stop prop on link clicks

--- src/script.js
+++ src/script.js
@@ -2,5 +2,12 @@ import { startColorMixer } from "./utils/colorMixer.js";
 import { startParallax } from "./utils/parallax.js";
 import { lerp } from "./utils/utils.js";
 
+//hack to prevent click on links to start the color generator
+document
+  .querySelectorAll("a")
+  .forEach((element) =>
+    element.addEventListener("click", (e) => e.stopPropagation())
+  );
+
 startParallax();
 startColorMixer();
